74366e10a70832694cdb487ad3a33b89400bc723
Mariam Kamel <mariamkamel98@hotmail.com>
2017-09-06 20:38:17 +0200

    FacebookContact

--- src/screen/ContactUs.js
+++ src/screen/ContactUs.js
@@ -12,7 +12,27 @@ import { Entypo } from '@expo/vector-icons';
 
 export default class ContactUs extends Component {
   render() {
-    
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() => Communications.email(['nabehny@gmail.com'], null, null, 'Subject', 'body text')}>
+          <View style={styles.holder}>
+            <Entypo name="mail" size={32} color="blue" />
+          </View>
+          <View>
+            <Text style={styles.text}>Send us mail</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Communications.web('https://www.facebook.com/nabehny/?ref=aymt_homepage_panel')}>
+          <View style={styles.holder}>
+            <Entypo name="facebook" size={32} color="blue" />
+          </View>
+          <View >
+            <Text style={styles.text}>Join Facebook</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
