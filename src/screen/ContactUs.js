100cd8a215ac6bf97d162869e35bdf20f6d3820a
Mariam Kamel <mariamkamel98@hotmail.com>
2017-09-06 20:36:53 +0200

    MailContact

---
+++ src/screen/ContactUs.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+
+import Communications from 'react-native-communications';
+import { Entypo } from '@expo/vector-icons';
+
+export default class ContactUs extends Component {
+  render() {
+    
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    backgroundColor: 'rgb(253,253,253)',
+  },
+  holder: {
+    paddingTop: 30,
+    flexDirection: 'column',
+  },
+  text: {
+    fontSize: 30,
+  },
+});
