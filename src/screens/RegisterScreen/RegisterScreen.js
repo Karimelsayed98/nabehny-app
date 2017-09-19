211c6a8f8b0cfe646f3976973635de071c07cbad
Khaled Mohamed <khal3d.mohamed@gmail.com>
2017-09-19 08:59:54 +0200

    Fixed Register Screen

--- src/screens/RegisterScreen/RegisterScreen.js
+++ src/screens/RegisterScreen/RegisterScreen.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { StyleSheet, View, Dimensions, TouchableOpacity, Text, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, View, Text, Keyboard, TouchableWithoutFeedback, Alert, Button } from 'react-native';
 import { Hoshi } from 'react-native-textinput-effects';
-import Validation from '/Users/mac/Documents/Projects/nabehny-app/src/functions/Validation';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationActions } from 'react-navigation';
+import { firebase } from '../../Firebase/Firebase';
+import Validation from '../../functions/Validation';
 
 export default class RegisterScreen extends React.Component {
   constructor(props) {
@@ -15,6 +18,35 @@ export default class RegisterScreen extends React.Component {
       scrollState: false,
     };
   }
+
+  _register() {
+    console.log('Register Button');
+    const { email, password, confirmPassword, fullname } = this.state;
+    if (Validation.RegisterValidation(email, password, confirmPassword, fullname)) {
+      firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then((user) => {
+          // Register Successful
+          user.updateProfile({
+            displayName: fullname,
+          }).then(() => {
+            // Update Successful
+            this.props.navigation.navigate('Login');
+          }).catch((error) => {
+            // Error in Update
+            console.log(error.name);
+            console.log(error.message);
+            Alert.alert('A Problem Occured, Please try again');
+          });
+        })
+        .catch((error) => {
+          // Error in Register
+          console.log(error.name);
+          console.log(error.message);
+          Alert.alert('A Problem Occured, Please try again');
+        });
+    }
+  }
+
   dismissKeyboard() {
     Keyboard.dismiss();
     this.setState({
@@ -22,22 +54,26 @@ export default class RegisterScreen extends React.Component {
     });
     console.log('keyboard dismissed');
   }
+
   enablescroll() {
     console.log('scroll enabled');
     this.setState({
       scrollState: true,
     });
   }
+
   render() {
     return (
-      <TouchableWithoutFeedback
-        onPress={() => this.dismissKeyboard()}
-      >
-        <KeyboardAwareScrollView scrollEnabled={this.state.scrollState}>
-
-          <View style={styles.registerform}>
+      <View style={styles.container}>
+        <Ionicons
+          name="ios-arrow-back-outline"
+          size={36}
+          onPress={() => this.props.navigation.dispatch(NavigationActions.back({}))}
+          style={{ alignSelf: 'flex-start', marginTop: 25, marginLeft: 15 }}
+        />
+        <TouchableWithoutFeedback onPress={() => this.dismissKeyboard()}>
+          <KeyboardAwareScrollView scrollEnabled={this.state.scrollState}>
             <Text style={styles.header}>REGISTER</Text>
-
             <Hoshi
               style={styles.effect}
               label={'Full Name'}
@@ -47,7 +83,6 @@ export default class RegisterScreen extends React.Component {
               value={this.state.fullname}
               borderColor={'#BA1818'}
               onFocus={() => this.enablescroll()}
-
             />
             <Hoshi
               style={styles.effect}
@@ -83,67 +118,49 @@ export default class RegisterScreen extends React.Component {
               onFocus={() => this.enablescroll()}
 
             />
-            <View style={styles.btnstyle}>
-              <TouchableOpacity
-                onPress={() => {
-                  Validation.RegisterValidation(
-                    this.state.email,
-                    this.state.password,
-                    this.state.confirmPassword,
-                    this.state.fullname);
-                }}
-              >
-                <Text style={styles.btntext}> SUBMIT </Text>
-              </TouchableOpacity>
-            </View>
-
-          </View>
-        </KeyboardAwareScrollView >
-
-      </TouchableWithoutFeedback >
+          </KeyboardAwareScrollView>
+        </TouchableWithoutFeedback>
+        <View
+          onPress={() => this._register()}
+          style={styles.btnStyle}
+        >
+          <Button
+            title="Submit"
+            onPress={() => this._register()}
+          />
+        </View>
+      </View>
     );
   }
 }
-const { height, width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  registerform: {
-    alignItems: 'center',
-    alignSelf: 'stretch',
-    backgroundColor: 'white',
-    padding: 15,
-    top: height * 0.13,
-  },
-  effect: {
-    alignSelf: 'stretch',
-    marginBottom: 10,
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
   },
   header: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    alignSelf: 'center',
     fontSize: 45,
     color: '#970F0F',
     fontFamily: 'Arial Rounded MT Bold',
     marginBottom: 60,
   },
-  btntext: {
-    fontSize: 20,
-    color: '#307EFF',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 10,
-
+  effect: {
+    marginBottom: 10,
+    marginHorizontal: 20,
   },
-  btnstyle: {
-    marginTop: 45,
-    borderRadius: 6,
+  btnStyle: {
     borderWidth: 1.5,
     alignContent: 'stretch',
     borderColor: '#F3F3F3',
-    top: height * 0.08,
-    width,
     padding: 6,
-    justifyContent: 'center',
     alignItems: 'center',
+  },
+  btnText: {
+    fontSize: 20,
+    color: '#307EFF',
   },
 });
