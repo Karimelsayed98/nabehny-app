158db90560bf7d7ad052c5e43d8555f49370393c
Khaled Mohamed <khal3d.mohamed@gmail.com>
2017-09-27 15:09:19 +0200

    Added Location Validation

--- src/functions/Validation.js
+++ src/functions/Validation.js
@@ -1,4 +1,4 @@
-import { Alert } from 'react-native';
+import { Alert, AsyncStorage } from 'react-native';
 
 export default class Validation {
   static RegisterValidation(mail, password, confirmPassword, fullname) {
@@ -36,5 +36,34 @@ export default class Validation {
     }
     Alert.alert(errorMsg);
     return false;
+  }
+
+  static pinLocationValidation(userStory, date) {
+    if (userStory && userStory.length < 200) {
+      Alert.alert('User Story should be empty or at least 200 letter');
+      return false;
+    }
+    const regexDate = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[0-2])[/-](201[0-9])$/;
+    if (date && !regexDate.test(date)) {
+      Alert.alert('Invalid Date');
+      return false;
+    }
+    return true;
+  }
+
+  static async checkLastEntry() {
+    const lastEntry = await AsyncStorage.getItem('lastEntryDate');
+    if (!lastEntry) return true;
+    const lastEntryDate = new Date(lastEntry);
+    lastEntryDate.setDate(lastEntryDate.getDate() + 30);
+    const today = new Date();
+    console.log(lastEntryDate, today);
+    if (today < lastEntryDate) { Alert.alert('30 Days need to pass to pin another location'); }
+    return (today >= lastEntryDate);
+  }
+
+  static isValidEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
   }
 }
