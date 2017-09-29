d4848a5eb2c7174de59010b502e385af77ddcc27
Khaled Mohamed <khal3d.mohamed@gmail.com>
2017-09-29 03:13:18 +0200

    Configured App.JS and App Icons

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar, Platform } from 'react-native';
 import { RootNavigator } from './src/Navigation/Navigation';
 import Test from './src/screens/Test';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View />
+      <View style={{ flex: 1 }}>
+        <StatusBar barStyle="dark-content" />
+        <RootNavigator style={{ marginTop: (Platform.OS === 'android') ? 20 : 0 }} />
+      </View>
     );
   }
 }
