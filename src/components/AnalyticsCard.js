f7b7baa70e0cd3acab186e5bf342881a7b82bd68
Khaled Mohamed <khal3d.mohamed@gmail.com>
2017-09-29 03:10:51 +0200

    Added Robbery header to Analytics Card

--- src/components/AnalyticsCard.js
+++ src/components/AnalyticsCard.js
@@ -4,20 +4,25 @@ import { View, StyleSheet, Text } from 'react-native';
 export default class AnalyticsCard extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.card}>
-          <Text style={styles.header}> PAST MONTH </Text>
-          <Text style={styles.number}> {this.props.pastMonth} </Text>
-        </View>
-        <View style={styles.vBreak} />
-        <View style={styles.card}>
-          <Text style={styles.header}> SAFETY RATE </Text>
-          <Text style={styles.number}> {this.props.safetyRate}<Text style={{ fontWeight: 'normal', fontSize: 14 }}>%</Text> </Text>
-        </View>
-        <View style={styles.vBreak} />
-        <View style={styles.card}>
-          <Text style={styles.header}> PAST WEEK </Text>
-          <Text style={styles.number}> {this.props.pastWeek} </Text>
+      <View style={{ backgroundColor: '#fff', alignItems: 'center' }}>
+        <Text style={{ fontSize: 12, marginVertical: 12 }}> Robbery Analytics for this Area </Text>
+        <View style={styles.container}>
+          <View style={styles.card}>
+            <Text style={styles.header}> PAST MONTH </Text>
+            <Text style={{ fontSize: 10, textAlign: 'center' }}>Robberies</Text>
+            <Text style={styles.number}> {this.props.pastMonth} </Text>
+          </View>
+          <View style={styles.vBreak} />
+          <View style={styles.card}>
+            <Text style={styles.header}> SAFETY RATE </Text>
+            <Text style={[styles.number, { marginTop: 14 }]}> {this.props.safetyRate}<Text style={{ fontWeight: 'normal', fontSize: 14 }}>%</Text> </Text>
+          </View>
+          <View style={styles.vBreak} />
+          <View style={styles.card}>
+            <Text style={styles.header}> PAST WEEK </Text>
+            <Text style={{ fontSize: 10, textAlign: 'center' }}>Robberies</Text>
+            <Text style={styles.number}> {this.props.pastWeek} </Text>
+          </View>
         </View>
       </View>
     );
@@ -34,7 +39,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginHorizontal: 20,
-    marginVertical: 15,
+    marginBottom: 5,
   },
   header: {
     fontSize: 12,
@@ -43,13 +48,13 @@ const styles = StyleSheet.create({
   },
   number: {
     fontSize: 34,
-    marginTop: 10,
+    marginTop: 2,
     fontWeight: 'bold',
     textAlign: 'center',
   },
   vBreak: {
     backgroundColor: '#f3f3f3',
     width: 1,
-    marginVertical: 15,
+    marginVertical: 10,
   },
 });
